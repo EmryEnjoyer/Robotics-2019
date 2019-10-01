ad63c880b74b034e9a7849b1c99a83c0e1fe63e5
Justice Roberts <ralphthedoge@gmail.com>
2019-09-30 23:00:20 -0400

    Initial upload of code
    Initial upload of code

---
+++ Robot 2019/src/org/usfirst/frc/team6189/robot/RobotClaw.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team6189.robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class RobotClaw extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Robot 2019/src/org/usfirst/frc/team6189/robot/RobotMap.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team6189.robot;
+
+public class RobotMap {
+
+}
